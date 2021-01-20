640db03cb757356603a2984d2c0db8e4b00448ec
Vee Satayamas <5ssgdxltv@relay.firefox.com>
2021-01-20 20:14:51 +0700

    feat(header): Add delete reader and delete line functions

--- headers/zstd_read_line.h
+++ headers/zstd_read_line.h
@@ -5,6 +5,14 @@
 
 #define BUF_SIZE 65536
 
-void *zstd_line_read_new(const char *zstd_file_path);
+typedef struct ZstdLineReader {
 
-const char *zstd_line_read(void *reader);
+} ZstdLineReader;
+
+struct ZstdLineReader *zstd_line_read_new(const char *zstd_file_path);
+
+char *zstd_line_read(struct ZstdLineReader *reader);
+
+void zstd_line_read_delete_line(char *line);
+
+void zstd_line_read_delete(struct ZstdLineReader *reader);
